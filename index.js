b48ae69cc7e7631ab0c735c527e137bcddd1fc9a
ashish ghimire <ashish.gh123@gmail.com>
2019-05-27 22:48:37 +0545

    Add handler for recipet comment

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ function sendStatus(req, resp){
     })
 }
 
-function getUsers(req,resp){
+function getComments(req,resp){
     dbClient
-    .select('email', 'password')
-    .table('users')
+    .select('userId', 'foodId', 'comment','timestamp')
+    .table('recipeComments')
     .then(data=>{
         resp.json({
             data : data
@@ -35,12 +35,14 @@ function getUsers(req,resp){
     })
 }
 
-function addUser(req,resp){
-    dbClient('users')
+function addComment(req,resp){
+    dbClient('recipeComments')
     .insert({
-        id : '7',
-        email: 'username',
-        password: 'password'
+        id : '1',
+        userId: '2',
+        foodId: '1',
+        comment:'It tastes good.',
+        timestamp:'2019-1-12'
     })
     .then(val =>{
         resp.json({
@@ -60,51 +62,55 @@ function addUser(req,resp){
 
 // handlers for recipe
 
-function getRecipes(req,resp){
-    dbClient
-    .select('name', 'description')
-    .table('recipe')
-    .then(data=>{
-        resp.json({
-            data : data
-        })
-    })
-}
+// function getRecipes(req,resp){
+//     dbClient
+//     .select('name', 'description')
+//     .table('recipe')
+//     .then(data=>{
+//         resp.json({
+//             data : data
+//         })
+//     })
+// }
 
 
-function addRecipe(req,resp){
-    dbClient('recipe')
-    .insert({
-        id : '7',
-        name: 'Toast',
-        description: 'Fried Toast'
-    })
-    .then(val =>{
-        resp.json({
-            status: 'success'
-        })
-    })
-    .catch(error => {
-        resp.json({
-            status: 'fail'
-        })
-        resp.json({
-            status: 'ok'
-        })
+// function addRecipe(req,resp){
+//     dbClient('recipe')
+//     .insert({
+//         id : '7',
+//         name: 'Toast',
+//         description: 'Fried Toast'
+//     })
+//     .then(val =>{
+//         resp.json({
+//             status: 'success'
+//         })
+//     })
+//     .catch(error => {
+//         resp.json({
+//             status: 'fail'
+//         })
+//         resp.json({
+//             status: 'ok'
+//         })
        
-    })
-}
+//     })
+// }
 
 
 
 
 express.get('/', sendStatus);
-express.get('/api/user', getUsers);
-express.post('/api/user', addUser);
+// express.get('/api/user', getUsers);
+// express.post('/api/user', addUser);
 
 // recipe
-express.get('/api/recipe', getRecipes);
-express.post('/api/recipe', addRecipe);
+// express.get('/api/recipe', getRecipes);
+// express.post('/api/recipe', addRecipe);
+
+// recipeComment
+express.get('/api/comment', getComments);
+express.post('/api/comment', addComment);
 
 
 
