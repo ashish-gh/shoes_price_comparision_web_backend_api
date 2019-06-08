a68c06cd401b639f3cf2c96d394ef510585fe672
ashish ghimire <ashish.gh123@gmail.com>
2019-06-08 22:24:35 +0545

    Update register function

--- index.js
+++ index.js
@@ -13,13 +13,23 @@ const jwt = require('jsonwebtoken');
 
 
 
-const dbClient= knex(config)
 
 //create an express instance /object
 
-// express.use(cors());
+express.use(cors());
 express.use(bodyParser.json());
 
+// this is client connection
+const dbClient= knex(config)
+const SECRET_KEY = 'secret_key';
+
+
+// get version
+
+function getVersion(req, res) {
+  // send me a version
+  res.json({version: '0.0.0'});           
+}
 
 
 // users
@@ -54,6 +64,59 @@ function registerUser(request, response) {
       })
   }
   
+// create a auth handler
+async function authenticate(request, response) {
+  
+  const email = request.body.email;
+  const passwordFromJSON = request.body.password;
+
+  const data = await dbClient.table('users').first('password').where('email', email);
+  if (!data) {
+    response.json({
+      status: 'fail',
+      message: 'User not found'
+    })
+  } else {
+    const password = data.password;
+    const isMatch = bcrypt.compareSync(passwordFromJSON, password);
+    if (isMatch) {
+      // password matched
+      response.json({
+        status: 'success',
+        accessToken: jwt.sign({
+          email: email
+        }, SECRET_KEY)
+      })
+   } else {
+     response.json({
+       status: 'fail',
+     })
+   }
+  } 
+}
+
+
+function _authenticate(token) { //token -> 123123789127389213
+  if (!token) {
+    return false;
+  }
+  try {
+    const payload = jwt.verify(token, SECRET_KEY);
+    return true;
+    // use payload if required
+  } catch(error) {
+    return false
+  }
+}
+
+
+function notAuthenticated(response) {
+  response.json({
+    status: 'fail',
+    message: 'Not Authenticated',
+    code: 404
+  });
+}
 
 
 
@@ -116,8 +179,8 @@ express.post('/api/comment', addComment);
 
 
 
-express.listen(8000, 'localhost', ()=> {
-    console.log("Server is running at", 8000)
+express.listen(8005, 'localhost', ()=> {
+    console.log("Server is running at", 8005)
 })
 
 
