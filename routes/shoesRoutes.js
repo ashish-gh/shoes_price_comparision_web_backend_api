9f6ff6345a2335d41c36f3416f72b7bf50a4cbbc
ashish ghimire <ashish.gh123@gmail.com>
2019-06-27 22:31:07 +0545

    Add shoes model

--- routes/shoesRoutes.js
+++ routes/shoesRoutes.js
@@ -4,12 +4,6 @@ const shoes = require('../controller/shoesController');
   
 
 app.post('/shoes', shoes.addShoes);
-// 
-app.get('/shoes', shoes.getShoes);
-app.delete('/shoes/:shoesId', shoes.deleteShoes);
-app.get('/shoes/:shoesId', shoes.getShoesById);
-app.get('/shoes/:shoesName', shoes.getShoesById);
-app.put('/shoes/:shoesId', shoes.updateShoes);
 
 
 module.exports = app;
