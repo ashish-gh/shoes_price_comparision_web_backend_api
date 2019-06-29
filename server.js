2e0fea56856046ce1837fc62cf9d7e48fa2555ca
ashish ghimire <ashish.gh123@gmail.com>
2019-06-29 15:32:30 +0545

    Add server file

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+app = express();
+
+
+const shoesRoutes = require('./routes/shoesRoutes');
+const storeRoutes = require('./routes/storeRoutes');
+
+
+//cors
+app.use(cors());
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// route for shoes
+app.use('/', shoesRoutes);
+
+// route for store
+app.use('/', storeRoutes);
+
+
+module.exports = app;
