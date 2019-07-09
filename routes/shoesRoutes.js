488c4b11d3a08fc9db5d2e40dfe7d28d9a3ae009
ashish ghimire <ashish.gh123@gmail.com>
2019-07-09 08:41:54 +0545

    Update shoes route controller and model

--- routes/shoesRoutes.js
+++ routes/shoesRoutes.js
@@ -2,12 +2,27 @@ const express = require('express');
 const app = express();
 const shoes = require('../controller/shoesController');
   
-
+// route to  add shoes
 app.post('/shoes', shoes.addShoes);
+
+// route to get shoes
 app.get('/shoes', shoes.getShoes);
+
+// route to get shoes by name
+app.get('/shoes/:shoesName/shoes', shoes.getShoesByName);
+
+// route to delete shoes
 app.delete('/shoes/:shoesId', shoes.deleteShoes);
+
+// route to update shoes
 app.put('/shoes/:shoesId', shoes.updateShoes);
+
+// route to get shoes by id
 app.get('/shoes/:shoesId', shoes.getShoesById);
+
+// route to upload image
+app.post('/upload', shoes.uploadImage);
+
 
 
 module.exports = app;
